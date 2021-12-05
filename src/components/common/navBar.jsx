117df0e73303748debc1b38595060c07eebea504
yashdkadam <kadamdyash55@gmail.com>
2021-12-05 14:36:08 +0530

    commited

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavBar = ({ user }) => {
+const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light justify-content-between">
       <Link className="navbar-brand" to="/" style={{ paddingLeft: 10 }}>
