d37272d90c63bccbdcd2c595abffed9c891f16d5
yashdkadam <kadamdyash55@gmail.com>
2022-12-22 09:52:23 +0530

    fixes

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React from "react";
 
 const NavBar = () => {
   return (
@@ -9,7 +9,7 @@ const NavBar = () => {
       </Link>
       <a
         className="navbar-brand "
-        href="http://localhost:3000/display"
+        href="/display"
         target="_blank"
         rel="noreferrer"
       >
